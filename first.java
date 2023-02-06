1cc7ec79ac5d9164c8c0de417694b941845f537d
Yet Chun Fong <yccc7171@gmail.com>
2023-02-05 16:57:44 -0800

    Added First file

---
+++ first.java
@@ -0,0 +1,11 @@
+public class first{
+
+    public int Addition(int a, int b){
+        int result = a + b; 
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Addition(1,2));
+    }
+}
